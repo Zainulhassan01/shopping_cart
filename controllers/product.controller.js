751972e660c3c9deae4a6b2dac09c86d9cc0247a
Dev <zainulhassan1018@gmail.com>
2023-07-19 05:51:11 -0700

    Zain: Completed controllers

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,18 +1,23 @@
-const products = require('../models/user.model')
+const products = require("../models/product.model");
 
-const getAllProducts = async (req,res) => {
-    const product = await products.find({})
-    res.send(product)
-}
+const getAllProducts = async (req, res) => {
+  const allProducts = await products.find({});
+  res.send(allProducts);
+};
 
 const createProduct = async (req, res) => {
-    const {name, price} = req.body
-    const data = new users({
-        name,
-        price
-    })
-    const newProduct = await data.save()
-    res.status(200).send(newProduct)
-}
+  const { name, price } = req.body;
+  const data = new products({
+    name,
+    price,
+  });
+  const newProduct = await data.save();
+  res.status(200).send(newProduct);
+};
 
-module.exports = {getAllProducts, createProduct}
+const getOneProduct = async (req, res) => {
+   const product = await products.findById(req.params.id)
+   res.status(200).send(product)
+};
+
+module.exports = { getAllProducts, createProduct, getOneProduct };
